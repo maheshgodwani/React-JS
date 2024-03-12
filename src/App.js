abc422131394379520b99432a7357bc97f0922e8
maheshgodwani <mhsmca5723@gmail.com>
2024-03-12 09:07:27 +0530

    Student Component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Demo from './demo.js'
 import Contact from './contact.js' 
-import student from './mca.js'
+const mca = require('./mca.json')
+
 
 function App() {
-  console.log(student);
   return (
     <>
   
     {/* <Demo /> */}
     {
-      student.map(function(student){
+      mca.map(function(student){
         return <Contact image={`https://cms.atmiya.edu.in/images/Stud_Photo/${student.AdmissionNo}.JPG`} name={`${student.StudentName}`} description={`${student.Division} - ${student.RollNo}`} />
       })
     }
