328bc54c93972b3ad5b4a0b147795fd86eea190d
Mahesh Godwani <143687176+maheshgodwani@users.noreply.github.com>
2024-03-22 10:58:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,28 @@ function App() {
         return <Contact image={`https://cms.atmiya.edu.in/images/Stud_Photo/${student.AdmissionNo}.JPG`} name={`${student.StudentName}`} description={`${student.Division} - ${student.RollNo}`} />
       })
     }
+
+    let [count, setCount] = useState(0);
+  const numbers = [1, 2, , 6, 8, 34, 66];
+  //console.log(numbers);
+
+  const Increment = () => setCount(count + 1);
+  const Decrement = () => setCount(count - 1);
+  const submit = () => setCount(0);
+  const reset= () => setCount(0);
+
+  for (let i = 0; i <= numbers.length; i++) {
+    console.log(i);
+  }
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={Increment}>+</button>{' '}
+      <button onClick={Decrement}>-</button>{' '}
+      <button onClick={reset}>reset</button>
+      <button onClick={submit}>submit</button>
+    </div>
+  );
   </>
   );
 }
